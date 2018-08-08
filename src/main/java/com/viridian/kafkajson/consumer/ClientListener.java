a13288322f911d002e984cd3ed8004bda2807b52
Isabraxas <isvar.vega@gmail.com>
2018-08-08 17:25:28 -0400

    funciona el envio de email con imagen adjunta por kafka

--- src/main/java/com/viridian/kafkajson/consumer/ClientListener.java
+++ src/main/java/com/viridian/kafkajson/consumer/ClientListener.java
@@ -1,7 +1,10 @@
 package com.viridian.kafkajson.consumer;
 
+import com.viridian.kafkajson.mail.EmailService;
+import com.viridian.kafkajson.mail.Mail;
 import com.viridian.kafkajson.model.Client;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
@@ -9,6 +12,7 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 import javax.imageio.ImageIO;
+import javax.mail.MessagingException;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -21,6 +25,10 @@ import java.io.InputStream;
 @Slf4j
 @Service
 public class ClientListener {
+
+    @Autowired
+    private EmailService emailService;
+
     @KafkaListener(topics = "${viridian.json.topic}")
     public void receive(@Payload Client data,
                         @Headers MessageHeaders headers) {
@@ -40,11 +48,21 @@ public class ClientListener {
             InputStream in = new ByteArrayInputStream(imageInByte);
             BufferedImage bImageFromConvert = ImageIO.read(in);
 
-            ImageIO.write(bImageFromConvert, "png", new File(
-                    "/home/isvar/Desktop/testimg-received.png"));
+            //Send email
+            Mail mail = new Mail();
+            mail.setFrom("no-reply@memorynotfound.com");
+            mail.setTo("isvar.vega@gmail.com");
+            mail.setSubject("Sending Email with Attachment Example");
+            mail.setContent("Content added with Spring Framework by send attached image test");
+            emailService.sendImageKafkaMessage(mail,imageInByte);
+            //End send email
+
+            ImageIO.write(bImageFromConvert, "png", new File("/home/isvar/Desktop/testimg-received.png"));
 
         } catch (IOException e) {
             System.out.println(e.getMessage());
+        } catch (MessagingException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/com/viridian/kafkajson/mail/EmailService.java
+++ src/main/java/com/viridian/kafkajson/mail/EmailService.java
@@ -2,13 +2,19 @@ package com.viridian.kafkajson.mail;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.InputStreamSource;
+import org.springframework.http.MediaType;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class EmailService {
@@ -33,6 +39,39 @@ public class EmailService {
 
         helper.addAttachment("attachment-document-image"+fileExtension, new ClassPathResource(fileName+fileExtension));
 
+        emailSender.send(message);
+
+    }
+
+    public void sendImageKafkaMessage(Mail mail, byte[] bytesfile) throws MessagingException {
+
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setSubject(mail.getSubject());
+        helper.setText(mail.getContent());
+        helper.setTo(mail.getTo());
+        helper.setFrom(mail.getTo());
+
+        //helper.addAttachment("attachment-document-image"+fileExtension, new InputStreamResource(inputStream));
+        //helper.addAttachment("attachment-document-image"+fileExtension,new ByteArrayResource(bytesfile), MediaType.APPLICATION_PDF.toString());
+        helper.addAttachment("attachment-document-image"+fileExtension,new ByteArrayResource(bytesfile), MediaType.IMAGE_PNG.toString());
+        emailSender.send(message);
+
+    }
+
+    public void sendPdfKafkaMessage(Mail mail, byte[] bytesfile) throws MessagingException {
+
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setSubject(mail.getSubject());
+        helper.setText(mail.getContent());
+        helper.setTo(mail.getTo());
+        helper.setFrom(mail.getTo());
+
+        //helper.addAttachment("attachment-document-image"+fileExtension, new InputStreamResource(inputStream));
+        helper.addAttachment("attachment-document-image"+.pdf,new ByteArrayResource(bytesfile), MediaType.APPLICATION_PDF.toString());
         emailSender.send(message);
 
     }
